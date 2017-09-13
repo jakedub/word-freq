175c89bd83f89be2539953ee91a29588ee9f3ca7
jakedub <jacob.w.moore@gmail.com>
2017-09-13 15:48:15 -0400

    committed

---
+++ README.md
@@ -0,0 +1,6 @@
+To calculate the frequency of words, you must:
+
+remove punctuation
+normalize all words to lowercase
+remove "stop words" -- words used so frequently they are ignored
+go through the file word by word and keep a count of how often each word is used
--- wordfreq.rb
+++ wordfreq.rb
@@ -4,19 +4,49 @@ class Wordfreq
     'were', 'will', 'with']
 
   def initialize(filename)
+    @data = File.read(filename).downcase
+    @data.gsub!(/[^a-z0-9\s]/i, ' ')
+    STOP_WORDS.each do |word|
+      @data.gsub!(/\b(?:#{word})\b/, '')
+    end
+    puts @data
   end
 
+#\b is the boundary
   def frequency(word)
+    counter = @data.scan(/\b(?:#{word})\b/).count
+    puts counter
   end
 
+# Hash.new is the same as {}
   def frequencies
+    @f_array = @data.split(' ')
+
+    @counts = Hash.new 0
+
+    @f_array.each do |word|
+      @counts[word] += 1
+    end
+    @counts
   end
 
+# take returns the number of values based on user input
   def top_words(number)
+    sorted_freq = frequencies.sort_by {|_word, freq| freq}
+    sorted_freq.reverse!
+    freq_array = []
+    sorted_freq.each do |word, freq|
+      freq_array << [word, freq]
+    end
+    freq_array.take(number)
   end
 
   def print_report
+    top_words(10).each do |word, number|
+      puts "#{word} | #{number}" + '*' *number
+    end
   end
+
 end
 
 if __FILE__ == $0
